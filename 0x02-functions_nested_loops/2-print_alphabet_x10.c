a316f5f6683f45c1ff6dc24282b3a7ec7513c268
Bossyyy <emmanuelapeagyei2@gmail.com>
2023-07-18 09:13:51 -0700

    printalph2.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * function that prints 10 times the alphabet in lowercase
+ * followed by new line
+ */
+
+void print_alphabet_x10(void
+		{ 
+		int i - 0;
+		char alph;
+
+		while (i <= 9)
+		{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+		_putchar(alph);
+		}
+		_putchar('\n');
+		i++;
+		}
+		}
