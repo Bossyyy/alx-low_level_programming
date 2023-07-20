a7ccd75cc9977bb5cdb7bbd28e93b4c996cffe75
Bossyyy <emmanuelapeagyei2@gmail.com>
2023-07-20 09:04:47 -0700

    4pmn

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void no 2 and 4
+ */
+
+void print_numbers(void)
+{
+        char a;
+
+        for (a = '0'; a <= '9'; a++)
+        {
+                if (!(c == '2' || c == '4'))
+			_putchar(c);
+        }
+        _putchar('\n');
+}
