d39963619b4096313b5ba3af706484435545b2f3
Bossyyy <emmanuelapeagyei2@gmail.com>
2023-07-24 08:26:03 -0700

    printreverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints in reverse
+ * @s: is a string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int u;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (u = longi; u > 0; u--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n')
+}
