afc888d72618795ac8db9f6a1689e847bbbe494f
Bossyyy <emmanuelapeagyei2@gmail.com>
2023-07-24 07:53:35 -0700

    returnlengthofstring

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen-returns length of string
+ * @k : string
+ * Return ; length
+ */
+int _strlen(char *k)
+{
+	int longi = 0;
+
+	while (*k != '\0')
+	{
+		longi++;
+		k++;
+	}
+	return (longi);
+}
