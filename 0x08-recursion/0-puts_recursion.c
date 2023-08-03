c12a9bf9a946a1ba1d8ed0110de2e7499e844f1f
Bossyyy <emmanuelapeagyei2@gmail.com>
2023-08-03 03:51:13 -0700

    print strings

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /** 
  * _puts_recursion -  function that prints a string,
  *  followed by a new line.
@@ -7,16 +6,16 @@
  *  Return: void
  *  Emmanuelapeagyei
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
+else
+{
+	_putchar('\n');
+}
+
+} /*@emma prints string*/
